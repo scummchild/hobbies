afe57763cbff3414d07c2229f1b2f913850819f9
Garison Spaeth <greesha@gmail.com>
2020-02-08 10:02:44 -0600

    refactor con_scraper into functions

--- con_scraper.py
+++ con_scraper.py
@@ -1,9 +1,9 @@
 """This script scrapes the galaxycon Minneapolis site and compares the result
-to a saved set of guests in a Mongodb database.  New additions and cancellations
-are found"""
+to a saved set of guests in a MongoDB database.  New additions and cancellations
+modify the database and send an email"""
 
 import argparse
-import sys
+from collections import namedtuple
 
 from requests_html import HTMLSession
 
@@ -12,8 +12,11 @@ from condb.guest import Guest
 import email_helper
 
 
+GuestDetail = namedtuple('GuestDetail', ['known_for', 'guest_url'])
+
+
 def parse_script_arguments() -> argparse.Namespace:
-    """Using argparse on the arguments passed into this script from the command line"""
+    """Use argparse on the arguments passed into this script from the command line"""
     parser = argparse.ArgumentParser(
         description='This script scrapes the galaxycon Minneapolis site and compares \
             the result to a saved set of guests in a Mongodb database. \
@@ -59,7 +62,7 @@ def send_alert(
         known_for: str,
         url: str,
         is_cancellation: bool):
-    """Helper function to determine how to call send_email"""
+    """Call send_email one way for new guests, another way for cancellations"""
 
     if not is_cancellation:
         subject = f'GalaxyCon addition: {name}'
@@ -87,49 +90,63 @@ def send_alert(
         message_html=html)
 
 
-def main():
-    # TODO: refactor into 3 pieces: get_current_guests, get_known_guests, compare_current_to_known
-    script_args = parse_script_arguments()
+def get_current_guests(con_url: str) -> dict:
+    """Parse con url and return dict of guests currently on the site"""
+    current_guests = {}
 
     session = HTMLSession()
-    resp = session.get("https://galaxycon.com/minneapolis-guests/")
+    resp = session.get(con_url)
+    guest_details = resp.html.find('div.rt-detail')
 
-    people = resp.html.find('div.rt-detail')
+    for guest_detail in guest_details:
+        name, known_for = guest_detail.text.split('\n')
+        guest_url = guest_detail.find('a', first=True).attrs['href']
 
-    if not people:
-        print('Unable to scrape con website')
-        sys.exit()
-    else:
-        mongo_init(alias='core', name=script_args.db_name, host=script_args.db_host,)
-        known_guest_names = [g.name for g in Guest.objects().only('name')]
-        web_guest_names = []
+        current_guests[name] = GuestDetail(
+            known_for=known_for, guest_url=guest_url)
 
-    for person in people:
-        guest_name, guest_known_for = person.text.split('\n')
-        guest_url = person.find('a', first=True).attrs['href']
+    return current_guests
 
-        web_guest_names.append(guest_name)
 
-        if guest_name not in known_guest_names:
-            new_guest = Guest(
-                name=guest_name,
-                known_for=guest_known_for,
-                url=guest_url)
+def compare_current_to_known(current_guests: dict) -> tuple:
+    """Helper function to determine how to call send_email"""
 
-            print(f'New addition! {new_guest.name}, known for {new_guest.known_for}')
+    known_guests = set(g.name for g in Guest.objects().only('name'))
 
-            new_guest.save()
+    new_guests = set(current_guests.keys()).difference(known_guests)
 
-            send_alert(
-                script_args=script_args,
-                name=new_guest.name,
-                known_for=new_guest.known_for,
-                url=new_guest.url,
-                is_cancellation=False)
+    cancelled_guests = known_guests.difference(current_guests.keys())
 
-    cancellations = [g for g in known_guest_names if g not in web_guest_names]
+    return (new_guests, cancelled_guests)
 
-    for cancelled_guest in cancellations:
+
+def add_new_guests(new_guests: set, current_guests: dict, script_args: argparse.Namespace):
+    """Save new guests to the database and call send_alert appropriately"""
+    for new_guest in new_guests:
+        guest = Guest(
+            name=new_guest,
+            known_for=current_guests[new_guest].known_for,
+            url=current_guests[new_guest].guest_url)
+
+        guest.save()
+
+        print(f'New addition! {guest.name}, known for {guest.known_for}')
+
+        send_alert(
+            script_args=script_args,
+            name=guest.name,
+            known_for=guest.known_for,
+            url=guest.url,
+            is_cancellation=False)
+
+
+def delete_cancelled_guests(cancelled_guests: set, script_args: argparse.Namespace):
+    """Delete cancelled guests from the database and call send_alert appropriately"""
+    for cancelled_guest in cancelled_guests:
+        Guest.objects(name=cancelled_guest).delete()
+
+        print(f'Oh no! {cancelled_guest} cancelled!')
+
         send_alert(
             script_args=script_args,
             name=cancelled_guest,
@@ -137,9 +154,24 @@ def main():
             url=None,
             is_cancellation=True)
 
-        print(f'Oh no! {cancelled_guest} cancelled!')
 
-        Guest.objects(name=cancelled_guest).delete()
+def main():
+    """Driving function when called from command line"""
+    script_args = parse_script_arguments()
+
+    current_guests = get_current_guests(
+        con_url='https://galaxycon.com/minneapolis-guests/')
+
+    assert current_guests
+
+    mongo_init(alias='core', name=script_args.db_name, host=script_args.db_host)
+
+    new_guests, cancelled_guests = compare_current_to_known(
+        current_guests)
+
+    add_new_guests(new_guests, current_guests, script_args)
+
+    delete_cancelled_guests(cancelled_guests, script_args)
 
 
 if __name__ == '__main__':
