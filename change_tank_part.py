a2b54266ccb577911e7348ee4bc1797579fa9e24
Garison Spaeth <greesha@gmail.com>
2020-08-16 21:32:40 -0500

    rough change tank part draft

--- change_tank_part.py
+++ change_tank_part.py
@@ -4,7 +4,7 @@ then show those values (press 1 to replace main tank water....alternate colors).
 'Replace main tank water? Y/N'
 '''
 
-from datetime import datetime
+from datetime import date
 from typing import cast
 
 from tankdb import mongo_setup as mongo_setup
@@ -13,30 +13,47 @@ from tankdb.equipmentpart import EquipmentPart
 
 mongo_setup.global_init()
 
-parts_list = set(f'{equipment.name} {part.name}' for equipment in TankEquipment.objects()
-    for part in equipment.parts)
+# parts_list = set(f'{equipment.name} {part.name}' for equipment in TankEquipment.objects()
+#     for part in equipment.parts)
 
-menu_options = {option_number: part for option_number,
-    part in enumerate(parts_list)}
+equipment_part_list = list(set((equipment.name, part.name) for equipment in TankEquipment.objects()
+    for part in equipment.parts))
+
+menu_option_list =  {option_number: part for option_number, part in list(enumerate([f'{part[0]} {part[1]}' for part in equipment_part_list]))}
+
+# menu_options = {option_number: part for option_number,
+#     part in enumerate(parts_list)}
 
 
 print('Welcome to the Turtle Tank Maintence System')
 print('Which part would you like to replace?')
 print('')
 # TODO: Print out in a nicer, colored format
-print(menu_options)
+print(menu_option_list)
 print('(Enter the number you would like to change)')
 
 menu_option = int(input())
 
-part_to_replace = menu_options.get(menu_option)
+equipment_name = equipment_part_list[menu_option][0]
 
-print(part_to_replace)
+equipment_found = TankEquipment.objects().filter(name=equipment_name).first()
 
-# for part in reversed(equipment_to_update.parts):
-#     count += 1
-#     if part.name == 'water' and not part.replaced_date:
-#         print(part.add_date)
-#         break
+for part in reversed(equipment_found.parts):
+    if part.name == equipment_part_list[menu_option][1] and not part.replaced_date:
+        replaced_date = date.today()
 
-# print(count)
+        part.replaced_date = replaced_date
+
+        new_part = EquipmentPart()
+        new_part.name = part.name
+        new_part.add_date = part.replaced_date
+        new_part.brand = part.brand
+        new_part.target_days_to_replace = part.target_days_to_replace
+
+        equipment_found.parts.append(new_part)
+
+        break
+
+equipment_found.save()
+
+
